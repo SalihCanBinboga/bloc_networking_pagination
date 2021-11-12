465b198723a0c3e59d522415e0cbc53671afd878
salihcanbinboga <salihcanbinboga@gmail.com>
2021-11-12 09:42:20 +0300

    develop: create list item loading component

---
+++ lib/app/presentation/screens/list_screen/components/list_loading_item.dart
@@ -0,0 +1,16 @@
+part of '../list_screen.dart';
+
+class _ListLoadingItem extends StatelessWidget {
+  const _ListLoadingItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SizedBox(
+        height: 24,
+        width: 24,
+        child: CircularProgressIndicator(strokeWidth: 1.5),
+      ),
+    );
+  }
+}
